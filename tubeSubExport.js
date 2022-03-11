5b4ee37cd074eb2e2284f070f43985608b003ad5
ajohntom <almasi.thomas.4@gmail.com>
2022-03-11 14:42:29 +0100

    Extension update
    jQuery updated.
    Add basic scroll based subscription loading.
    Fix HTML element creation problem (extension not showing up).
    Add instruction text, and logging upon errors.
    Add error handling several places.

--- tubeSubExport.js
+++ tubeSubExport.js
@@ -1,9 +1,31 @@
     //Registering on the DOM done an IIFE
     $(function () {
-        var primaryContainer = $("#primary");
-        primaryContainer = primaryContainer.find("ytd-section-list-renderer");
-        primaryContainer = primaryContainer.find("ytd-item-section-renderer");
-        //primaryContainer = primaryContainer.find("#grid-container");
+		//Because it takes time to load content via scripts
+		setTimeout(()=>{
+			buildUI();
+		}, 1000);
+    });
+
+	function buildUI(){
+		var primaryContainer = $("#primary");
+		var instructionContainer = $('<div/>', {
+            id: 'sub-exporter-container',
+            "class": "style-scope",
+        }).prependTo(primaryContainer);
+		var instructionTextContainer = $('<div/>', {
+            id: 'sub-instruction',
+            "class": "style-scope"
+        }).prependTo(instructionContainer);
+		var instructionText = $('<span/>', {
+            id: 'sub-instruction-text',
+            "class": "style-scope",
+        }).prependTo(instructionTextContainer);
+		instructionText.html("Upon clicking Download button, extension will try to automatically scroll down to load every subscription. If the XML does not contain your subscriptions correctly, please scroll down, until every subscription is loaded!");
+		var logText = $('<span/>', {
+            id: 'sub-log-text',
+            "class": "style-scope",
+        }).prependTo(instructionTextContainer);
+
         var container = $('<div/>', {
             id: 'sub-exporter-container',
             "class": "style-scope",
@@ -17,9 +39,10 @@
             "class": "style-scope",
         }).prependTo(button);
         buttonText.html("download subscriptions xml");
-        button.click(function () {
+        button.click(() => {
+			clearLog();
             getSubsXML();
-        }.bind(this));
+        });
         var progressBarContainer = $('<div/>', {
             id: 'sub-exporter-progressbar-container',
             "class": "style-scope hidden",
@@ -36,10 +59,53 @@
             max: 100,
             value: 0
         }).prependTo(progressBarContainer);
-    });
+		//Clear log, to make sure...
+		clearLog();
+		//Dark mode handling (text colors can vary...)
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			$(progressBarLabel).css("color", "#ffffff");
+		}
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+			var newColorScheme = event.matches ? "dark" : "light";
+			switch(newColorScheme){
+				case "dark":
+					$(progressBarLabel).css("color", "#ffffff");
+					break;
+				case "light":
+					$(progressBarLabel).css("color", "#000");
+					break;
+			}
+		});
+	}
+
+	function getAllSubs(){
+		return new Promise((resolve, reject)=>{
+			var prevHeight = 0;
+			var isFinished = false;
+			var cI = setInterval(() => {
+				if(!isFinished){
+					$(window).scrollTop(prevHeight + $("#primary").height());
+					setTimeout(()=>{
+						prevHeight = $("#primary").height();
+						if($("#primary").height() <= prevHeight){
+							isFinished = true;
+							$(window).scrollTop(0);
+							resolve("Done");
+						}
+					},200)
+				}
+				else{
+					clearInterval(cI);
+				}
+			},250);
+		});
+		
+	}
 
     //Get sub names and direct links
-    function getSubsXML() {
+    async function getSubsXML() {
+	try{
+		await getAllSubs();
         //Reset meter
         $("#sub-exporter-progressbar").attr("value", 0);
         //Show progress bar upon fetch start
@@ -48,7 +114,7 @@
         var ytSubsRow = $("ytd-channel-renderer");
         var channelInfos = [];
         var channelLinks = {};
-        var defs = [];
+        var promiseArray = [];
         for (var i = 0; i < ytSubsRow.length; i++) {
             channelInfos.push(ytSubsRow[i].childNodes[2].childNodes[1]);
         }
@@ -61,56 +127,92 @@
         //Channel name + links ready --> fetch canonical links where it's needed
 
         //Canonical link fetch where needed
+		try{
         for (var key in channelLinks) {
             if (!(/\/channel\//i.test(channelLinks[key]))) {
-                var dfd = $.Deferred();
-                defs.push(dfd);
-                $.ajax({
+				promiseArray.push($.ajax({
                     url: channelLinks[key],
                     context: {
-                        def: dfd,
                         channelName: key,
                         channelLinks: channelLinks,
                         channelConunt: channelConunt,
                     },
                     success: function (data) {
                         var html = $.parseHTML(data);
+						var canonicalFound = false;
                         for (var i = 0; i < html.length; i++) {
                             if (html[i].rel == "canonical") {
                                 this.channelLinks[this.channelName] = html[i].href;
-                                this.def.resolve("Fetched");
                                 var value = parseFloat($("#sub-exporter-progressbar").attr("value"));
                                 $("#sub-exporter-progressbar").attr("value", value + (1/this.channelConunt*100));
+								canonicalFound = true;
+								break;
                             }
                         }
-                    }
-                });
+						if(!canonicalFound){
+							this.channelLinks[this.channelName] = undefined;
+							var value = parseFloat($("#sub-exporter-progressbar").attr("value"));
+							$("#sub-exporter-progressbar").attr("value", value + (1/this.channelConunt*100));
+							addLogLine(`ERROR: Channel link fetching failed! Channel name: ${this.channelName}. Will be excluded from XML!`);
+						}
+						
+                    },
+					error: function (xhr, err){
+						console.error(err);
+					}
+                }));
             }
             else{
                 var value = parseFloat($("#sub-exporter-progressbar").attr("value"));
                 $("#sub-exporter-progressbar").attr("value", value + (1/channelConunt*100));
             }
         }
-        $.when.apply($, defs).done(function () {
+		}
+		catch(err){
+			console.error(err);
+		}
+
+		//Check if all fetched
+        Promise.all(promiseArray)
+		.then(() => {
             parseFeedLinks(channelLinks);
             //Hide progress bar upon fetch done
             $("#sub-exporter-progressbar-container").toggleClass("hidden");
-        }.bind(this));
+        })
+		.catch((err)=>{
+			console.error(err);
+			//Handle error
+			addLogLine(`ERROR: During channel link fetching process! XML can not be generated!`);
+			//Hide progress bar upon fetch done
+            $("#sub-exporter-progressbar-container").toggleClass("hidden");
+		});
+	}
+	catch(err)
+	{
+		console.error(err);
+	}
     }
 
     //Modifies channel URLs to point to their feed
     function parseFeedLinks(channelLinks) {
         var xmlUrl = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+		//Bad links contain an array of links, where canonical link is not available
+		var badLinks = [];
         for (var key in channelLinks) {
-            var channelId = channelLinks[key].split("/").pop();
-            channelLinks[key] = xmlUrl + channelId;
+			if(channelLinks[key] == undefined){
+				delete channelLinks[key];
+			}
+			else{
+				var channelId = channelLinks[key].split("/").pop();
+				channelLinks[key] = xmlUrl + channelId;
+			}
         }
         assembleXML(channelLinks);
     }
 
     function assembleXML(channelLinks) {
         var XML = [];
-        var pre = '<opml version="1.1">\n<body><outline text="YouTube – subscriptions" title="YouTube – subscriptions">';
+        var pre = '<opml version="1.1">\n<body><outline text="YouTube - subscriptions" title="YouTube - subscriptions">';
         var post = '</outline></body></opml>';
         var preCh = '<outline ';
         var postCh = '/>';
@@ -148,3 +250,16 @@
         element.click();
         document.body.removeChild(element);
     };
+
+	function addLogLine(lineContent){
+		
+		var logLine = $('<div/>', {
+            id: 'sub-log-text-line',
+            "class": "style-scope",
+        }).appendTo($("#sub-log-text"));
+		logLine.html(new Date().toLocaleString() + " - " + lineContent);
+	}
+
+	function clearLog(){
+		$("#sub-log-text").html("");
+	}
